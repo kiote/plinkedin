3408179c58aab468a51df51c327dff8c18da3139
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2014-08-10 01:05:16 +0400

    first commit

---
+++ server.py
@@ -0,0 +1,39 @@
+import tornado.ioloop
+import tornado.web
+import logging
+import sys
+
+from tornado.gen import coroutine
+from tornado.httpclient import AsyncHTTPClient, HTTPRequest, HTTPError
+
+logging.warning('tornado')
+
+class MainHandler(tornado.web.RequestHandler):
+  def initialize(self):
+    self.client = AsyncHTTPClient()
+
+  @coroutine
+  def get(self):
+    yield self.handle()
+
+  @coroutine
+  def handle(self):
+    logging.warning(self.request.method)
+
+    request = HTTPRequest(
+        url='http://yandex.ru',
+        method=self.request.method
+    )
+    
+    try:
+      response = yield self.client.fetch(request)
+      logging.warning(response)
+    finally:
+      self.write("response")
+      self.finish() 
+
+application = tornado.web.Application([(r"/", MainHandler),])
+
+if __name__ == "__main__":
+  application.listen(8888)
+  tornado.ioloop.IOLoop.instance().start()
