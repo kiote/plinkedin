89f497fc9c74893870655fc5425210a04c5b65f1
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2014-08-12 00:03:31 +0400

    code exactly as mikedory #2

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
+#    Sample main.py Tornado file
+#    (for Tornado on Heroku)
+#
+#    Author: Mike Dory | dory.me
+#    Created: 11.12.11 | Updated: 06.02.13
+#    Contributions by Tedb0t, gregory80
+#
+# ------------------------------------------
+
+#!/usr/bin/env python
 import os.path
 import tornado.escape
 import tornado.httpserver
@@ -24,14 +34,23 @@ class Application(tornado.web.Application):
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
+# the main page
 class MainHandler(tornado.web.RequestHandler):
-  def get(self, q):
-    self.render(
-        "main.html",
-        page_title='Heroku Funtimes',
-        page_heading='Hi!'
-    )
+    def get(self, q):
+        if 'GOOGLEANALYTICSID' in os.environ:
+            google_analytics_id = os.environ['GOOGLEANALYTICSID']
+        else:
+            google_analytics_id = False
 
+        self.render(
+            "main.html",
+            page_title='Heroku Funtimes',
+            page_heading='Hi!',
+            google_analytics_id=google_analytics_id,
+        )
+
+
+# RAMMING SPEEEEEEED!
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
