325b496ecfe5d8d8203dfeadd8f6abed52e864a6
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2014-08-12 19:14:28 +0400

    options in sepearate file

--- main.py
+++ main.py
@@ -9,15 +9,9 @@ from handlers.auth import AuthHandler
 from handlers.main import MainHandler
 
 # import and define tornado-y things
-from tornado.options import define, options
+from tornado.options import options
 
-# options
-define("port", default=5000, help="run on the given port", type=int)
-define("state", default="abcd", help="Used to prevent CSRF", type=str)
-define("redirect_url", default="http://protected-waters-8043.herokuapp.com/", \
-  help="OAuth2 redirect URL", type=str)
-define("client_id", default="wrong", help="Value of API key", type=str)
-define("linkedin_oauth", default="wrong", help="LinkedIn OAuth URL", type=str)
+import options
 
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -41,8 +35,7 @@ def main():
 
   http_server = tornado.httpserver.HTTPServer(Application())
   http_server.listen(tornado.options.options.port)
-  # logging.warn(tornado.options.options.state)
-  # start it up
+
   tornado.ioloop.IOLoop.instance().start()
 
 
---
+++ options.py
@@ -0,0 +1,9 @@
+from tornado.options import define
+
+# options
+define("port", default=5000, help="run on the given port", type=int)
+define("state", default="abcd", help="Used to prevent CSRF", type=str)
+define("redirect_url", default="http://protected-waters-8043.herokuapp.com/", \
+  help="OAuth2 redirect URL", type=str)
+define("client_id", default="wrong", help="Value of API key", type=str)
+define("linkedin_oauth", default="wrong", help="LinkedIn OAuth URL", type=str)
