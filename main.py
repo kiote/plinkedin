27819ae923ba0d3db53bfe7b42ec633c5f3e4125
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2014-08-16 11:51:44 +0400

    import option – back

--- main.py
+++ main.py
@@ -12,31 +12,34 @@ from handlers.main import MainHandler
 from tornado.options import options
 
 import logging
+import options
+
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class Application(tornado.web.Application):
-  def __init__(self):
-    handlers = [
-      (r"/", MainHandler),
-      (r"/auth", AuthHandler)
-    ]
-    settings = dict(
-      template_path=os.path.join(os.path.dirname(__file__), "templates"),
-      static_path=os.path.join(os.path.dirname(__file__), "static"),
-      debug=True,
-    )
-    tornado.web.Application.__init__(self, handlers, **settings)
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/auth", AuthHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
 
 def main():
-  tornado.options.parse_command_line()
-  tornado.options.parse_config_file("config.cnf")
+    tornado.options.parse_command_line()
+    tornado.options.parse_config_file("config.cnf")
 
-  http_server = tornado.httpserver.HTTPServer(Application())
-  http_server.listen(tornado.options.options.port)
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(tornado.options.options.port)
 
-  tornado.ioloop.IOLoop.instance().start()
+    tornado.ioloop.IOLoop.instance().start()
 
 
 if __name__ == "__main__":
-  main()
+    main()
